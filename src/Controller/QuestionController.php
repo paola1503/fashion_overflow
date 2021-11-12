17bdc12c3e07fbad9dea1120aee86a45815eb2fb
paola <paola.petri@aircury.com>
2021-11-12 13:09:02 +0100

    Modified files

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(Environment $twigEnvironment)
+    {
+        /*
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+        */
+
+        return $this->render('question/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     */
+    public function show($slug)
+    {
+        $answers = [
+            'Wear shoes that are the same color as your bag!',
+            'If your outfit has a lot of color, wear black or white shoes; if your outfit is made of neutral colors, wear bold shoes.',
+            'Invest on high-quality shoes and they will go with everything!',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
+    }
+}
