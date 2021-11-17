cca40d726dfece3755d77fe8c459ddefeec29e61
paola <paola.petri@aircury.com>
2021-11-17 16:46:55 +0100

    Modified files

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -2,12 +2,35 @@
 
 namespace App\Service;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
 class MarkdownHelper
 {
+    private $markdownParser;
+    private $cache;
+    private $isDebug;
+    private $logger;
+
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger)
+    {
+        $this->markdownParser=$markdownParser;
+        $this->cache=$cache;
+        $this->isDebug=$isDebug;
+        $this->logger=$markdownLogger;
+    }
     public function parse(string $source): string
     {
-        return $cache->get('markdown_'.md5($source),function () use ($source,$markdownParser){
-            return $markdownParser->transformMarkdown($source);
+        if (stripos($source, 'shoe')!==false){
+            $this->logger->info('Wow!');
+        }
+        if($this->isDebug)
+        {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+        return $this->cache->get('markdown_'.md5($source),function () use ($source){
+            return $this->markdownParser->transformMarkdown($source);
         });
     }
 }
